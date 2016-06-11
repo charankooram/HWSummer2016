533c167d668098a97204fdb709f56c8bb38effd8
Robert Crews <rcrews@hortonworks.com>
2016-06-11 14:42:58 -0700

    Resolved most pylint recommendations.

--- python/mirror-dirs.py
+++ python/mirror-dirs.py
@@ -18,39 +18,47 @@ Use tar.bz2 to compress the resulting JSON:
     -rw-r--r--  1 rcrews  staff   192M Jun 11 11:13 docs.hortonworks.com-json.zip
 """
 
+import argparse
+import codecs
+import json
+import logging
+import os
+import re
+import time
+import urllib.parse
+import lxml.html
+
 __version__ = '0.0.5'
 
-import argparse, codecs, html, json, logging, lxml.html, os, re, time, urllib.parse
-
 # Get command-line arguments
-argparser = argparse.ArgumentParser()
-logfile, _ = os.path.splitext(os.path.basename(__file__))
-logfile += '.log'
-argparser.add_argument('-l', '--logfile', default=logfile,
-    help='the log file, defaults to ./' + logfile)
-argparser.add_argument('-v', '--verbosity', type=int, default=2,
-    help='message level for log', choices=[1, 2, 3, 4, 5])
-argparser.add_argument('in_dir',
-    help='directory containing text and HTML files')
-argparser.add_argument('out_dir',
-    help='nonexisting directory where JSON files will be written')
-args = argparser.parse_args()
+ARGPARSER = argparse.ArgumentParser()
+LOGFILE, _ = os.path.splitext(os.path.basename(__file__))
+LOGFILE += '.log'
+ARGPARSER.add_argument('-l', '--logfile', default=LOGFILE,
+                       help='the log file, defaults to ./' + LOGFILE)
+ARGPARSER.add_argument('-v', '--verbosity', type=int, default=2,
+                       help='message level for log', choices=[1, 2, 3, 4, 5])
+ARGPARSER.add_argument('in_dir',
+                       help='directory containing text and HTML files')
+ARGPARSER.add_argument('out_dir',
+                       help='nonexisting directory where JSON files will be written')
+ARGS = ARGPARSER.parse_args()
 
 # In JSON, include the URL only from the web root. We can add the
 # authority (e.g., the domain, i.e., docs.hortonworks.com) in
 # JavaScript when reading the JSON
-if args.in_dir.endswith('/'):
-    args.in_dir = args.in_dir[:-1] # Remove last character
+if ARGS.in_dir.endswith('/'):
+    ARGS.in_dir = ARGS.in_dir[:-1] # Remove last character
 
 # https://docs.python.org/3/library/logging.html#levels
-args.verbosity *= 10 # debug, info, warning, error, critical
+ARGS.verbosity *= 10 # debug, info, warning, error, critical
 
 # Set up logging
 # To monitor progress, tail the log file or use /usr/bin/less in F mode
 logging.basicConfig(
     format='%(asctime)s %(levelname)8s %(message)s', filemode='w',
-    filename=args.logfile)
-logging.getLogger().setLevel(args.verbosity)
+    filename=ARGS.logfile)
+logging.getLogger().setLevel(ARGS.verbosity)
 
 
 def mirror_dirs(src_dir, dest_dir):
@@ -58,7 +66,7 @@ def mirror_dirs(src_dir, dest_dir):
 
     # Fatal error if dest_dir exists. User is forced to either move or
     # delete the existing output directory before continuing.
-    os.mkdir(dest_dir)    
+    os.mkdir(dest_dir)
     logging.info(dest_dir)
 
     for item in os.listdir(src_dir):
@@ -84,13 +92,13 @@ def mirror_dirs(src_dir, dest_dir):
 
             # Use different parsers for files with different extensions
             if extension == '.txt':
-                pydict = text_to_json(src_path, args.in_dir)
+                pydict = text_to_json(src_path, ARGS.in_dir)
             else:
-                pydict = html_to_json(src_path, args.in_dir)
+                pydict = html_to_json(src_path, ARGS.in_dir)
 
             # Write JSON as UTF-8
-            with codecs.open(dest_path, mode='w', encoding='UTF-8') as fp:
-                json.dump(pydict, fp, ensure_ascii=False)
+            with codecs.open(dest_path, mode='w', encoding='UTF-8') as file_handle:
+                json.dump(pydict, file_handle, ensure_ascii=False)
 
 
 def text_to_json(text_file, path_prefix=''):
@@ -100,8 +108,8 @@ def text_to_json(text_file, path_prefix=''):
     datetime = get_datetime(text_file)
 
     # Read text files as cp1252, ignoring errors
-    with codecs.open(text_file, mode='r', encoding='cp1252', errors='ignore') as fp:
-        content = fp.read()
+    with codecs.open(text_file, mode='r', encoding='cp1252', errors='ignore') as file_h:
+        content = file_h.read()
 
     # Convert file system path to URL syntax
     trimed_path = trim_prefix(text_file, path_prefix)
@@ -126,7 +134,7 @@ def text_to_json(text_file, path_prefix=''):
 def normalize_whitespace(text):
     """Collapses whitespace runs to a single space, trims leading and trailing spaces."""
     text = re.sub(r'\s+', ' ', text)
-    text = text.strip();
+    text = text.strip()
     return text
 
 
@@ -145,41 +153,41 @@ def parse_path(path):
 
     # Look for paths like HDPDocuments/SS1/SmartSense-1.2.2/bk_smartsense_admin
     # and HDPDocuments/HDP2/HDP-2.3-yj/bk_cluster-planning-guide
-    m1 = re.search(r"""
+    std_path = re.search(r"""
         HDPDocuments / ([^/]+) / \w+ - ([-.\w]+) / (?: ds_ | bk_ ) ([^/]+) /
         """, path, flags=re.X)
-    if m1 and m1.group(1) and m1.group(2) and m1.group(3):
-        path_metadata['product'] = m1.group(1)
-        path_metadata['release'] = m1.group(2)
-        path_metadata['booktitle'] = m1.group(3)
+    if std_path and std_path.group(1) and std_path.group(2) and std_path.group(3):
+        path_metadata['product'] = std_path.group(1)
+        path_metadata['release'] = std_path.group(2)
+        path_metadata['booktitle'] = std_path.group(3)
 
     # Look for paths like HDPDocuments/HDP2/HDP-2.2.4-Win/bk_Clust_Plan_Gd_Win
-    m2 = re.search(r"""
+    win_new_path = re.search(r"""
         HDPDocuments / HDP[12] / \w+ - ([.\w]+) - Win / (?: ds_ | bk_ ) ([^/]+) /
         """, path, flags=re.X)
-    if m2 and m2.group(1) and m2.group(2):
+    if win_new_path and win_new_path.group(1) and win_new_path.group(2):
         path_metadata['product'] = 'HDP-Win'
-        path_metadata['release'] = m2.group(1)
-        path_metadata['booktitle'] = m2.group(2)
+        path_metadata['release'] = win_new_path.group(1)
+        path_metadata['booktitle'] = win_new_path.group(2)
 
     # Look for paths like HDPDocuments/HDP1/HDP-Win-1.1/bk_cluster-planning-guide
-    m3 = re.search(r"""
+    win_old_path = re.search(r"""
         HDPDocuments / HDP[12] / HDP-Win - ([.\w]+) / (?: ds_ | bk_ ) ([^/]+) /
         """, path, flags=re.X)
-    if m3 and m3.group(1) and m3.group(2):
+    if win_old_path and win_old_path.group(1) and win_old_path.group(2):
         path_metadata['product'] = 'HDP-Win'
-        path_metadata['release'] = m3.group(1)
-        path_metadata['booktitle'] = m3.group(2)
+        path_metadata['release'] = win_old_path.group(1)
+        path_metadata['booktitle'] = win_old_path.group(2)
 
     # Look for paths like HDPDocuments/Ambari-1.5.0.0/bk_ambari_security
-    m4 = re.search(r"""
+    ambari_path = re.search(r"""
         HDPDocuments / Ambari - ([.\w]+) / (?: ds_ | bk_ ) ([^/]+) /
         """, path, flags=re.X)
-    if m4 and m4.group(1) and m4.group(2):
+    if ambari_path and ambari_path.group(1) and ambari_path.group(2):
         path_metadata['product'] = 'Ambari'
-        path_metadata['release'] = m4.group(1)
-        path_metadata['booktitle'] = m4.group(2)
-    
+        path_metadata['release'] = ambari_path.group(1)
+        path_metadata['booktitle'] = ambari_path.group(2)
+
     return path_metadata
 
 
@@ -206,12 +214,13 @@ def get_text(element):
     # https://www.w3.org/TR/CSS21/sample.html#q22.0 and
     # https://developer.mozilla.org/en-US/docs/Web/HTML/Block-level_elements
     html_blocks = ['address', 'article', 'aside', 'blockquote', 'body',
-        'canvas', 'center', 'dd', 'dir', 'div', 'dl', 'dt', 'fieldset',
-        'figcaption', 'figure', 'footer', 'form', 'frame', 'frameset', 'h1',
-        'h2', 'h3', 'h4', 'h5', 'h6', 'header', 'hgroup', 'hr', 'html',
-        'li', 'main', 'menu', 'nav', 'noframes', 'noscript', 'ol', 'output',
-        'p', 'pre', 'section', 'table', 'tfoot', 'ul', 'video', 'caption',
-        'tbody', 'thead']
+                   'canvas', 'center', 'dd', 'dir', 'div', 'dl', 'dt',
+                   'fieldset', 'figcaption', 'figure', 'footer', 'form',
+                   'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6',
+                   'header', 'hgroup', 'hr', 'html', 'li', 'main', 'menu',
+                   'nav', 'noframes', 'noscript', 'ol', 'output', 'p',
+                   'pre', 'section', 'table', 'tfoot', 'ul', 'video',
+                   'caption', 'tbody', 'thead']
 
     if element.text:
         text += element.text
@@ -230,8 +239,9 @@ def get_text(element):
 
 def html_to_json(html_path, path_prefix):
     """Parse HTML and return a dictionary that can be converted to a JSON file."""
-    
+
     dictionary = {}
+    section_numbering_characters = '0123456789.  ' # nonbreaking space included
 
     # Parse page
     etree = lxml.html.parse(html_path)
@@ -253,19 +263,19 @@ def html_to_json(html_path, path_prefix):
         title = 'no title'
     title = normalize_whitespace(title)
     title = trim_prefix(title, 'Chapter')
-    title = title.lstrip('0123456789.  ') # nonbreaking space included
+    title = title.lstrip(section_numbering_characters)
     dictionary['title'] = title
 
     # Get text from areas representing priority content
     # Matches in this content should cause the document to rank higher
     priority_content = ['//h1', '//h2', '//h3', '//h4', '//h5', '//h6',
-            '//title', "//caption", "//figcaption"]
+                        '//title', "//caption", "//figcaption"]
     priority_text_list = []
     for xpath in priority_content:
         for elem in etree.xpath(xpath):
             if isinstance(elem, lxml.html.HtmlElement):
                 p_text = get_text(elem)
-                p_text = p_text.lstrip('0123456789.  ') # nonbreaking space included
+                p_text = p_text.lstrip(section_numbering_characters)
                 priority_text_list.append(p_text)
     if 'description' in dictionary:
         priority_text_list.append(dictionary['description'])
@@ -286,7 +296,7 @@ def html_to_json(html_path, path_prefix):
 
     # Convert file system path to URL syntax
     trimed_path = trim_prefix(html_path, path_prefix)
-    url_escaped_path = urllib.parse.quote(trimed_path)   
+    url_escaped_path = urllib.parse.quote(trimed_path)
     dictionary['url'] = url_escaped_path
 
     # Get file modification date
@@ -302,4 +312,4 @@ def html_to_json(html_path, path_prefix):
 
 # Command-line interface
 if __name__ == '__main__':
-    mirror_dirs(args.in_dir, args.out_dir)
+    mirror_dirs(ARGS.in_dir, ARGS.out_dir)
